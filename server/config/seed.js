75e7e693f9110039cba0ba1d17be9c176e902e9a
Juan David Reina <judareiro@gmail.com>
2019-11-19 06:37:38 -0500

    Moviendo todo fuera de la carpeta server

--- server/config/seed.js
+++
@@ -1,5 +0,0 @@
-// ================================
-// CONFIGURACION DE LA SEMILLA PARA EL TOKEN DE LOGUEO
-// ================================
-
-module.exports.SEED = '#Arconte@-L1t3l4nt35@_2019-!';
