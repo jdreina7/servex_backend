eae72966e2bd3bd59c07d793dd4afca8feaa062d
Juan David Reina <judareiro@gmail.com>
2019-11-19 06:57:29 -0500

    Moviendo todo fuera de la carpeta server

--- routes/upload.js
+++ routes/upload.js
@@ -58,7 +58,7 @@ app.put('/:tipo/:id', (req, res) => {
 
     // Mover a un path temporal
     // var path = `./server/server/server/uploads/${ tipo }/${ nombre_archivo }`;
-    var path = `./uploads/${ tipo }/${ nombre_archivo }`;
+    var path = __dirname + `./uploads/${ tipo }/${ nombre_archivo }`;
 
     console.log('EL dirname Juan: ' + __dirname);
     console.log('EL path Juan: ' + path);
@@ -99,7 +99,7 @@ app.put('/:tipo/:id', (req, res) => {
                 console.log(user.usr_img);
 
                 // var pathViejo = './server./uploads/users/' + user.usr_img; =========OJO DESCOMENTAR ESTA LINEA PARA LOCAL
-                var pathViejo = './uploads/users/' + user.usr_img;
+                var pathViejo = __dirname + './uploads/users/' + user.usr_img;
 
                 // console.log('el path viejo es: ' + pathViejo+'espaciopegado');
 
@@ -179,7 +179,7 @@ app.put('/:tipo/:id', (req, res) => {
                 console.log(client.client_logo);
 
                 // var pathViejo = './server./uploads/clients/' + client.client_logo;
-                var pathViejo = './uploads/clients/' + client.client_logo;
+                var pathViejo = __dirname + './uploads/clients/' + client.client_logo;
 
                 // console.log('el path viejo es: ' + pathViejo+'espaciopegado');
 
@@ -259,7 +259,7 @@ app.put('/:tipo/:id', (req, res) => {
                 console.log(category.cat_img);
 
                 // var pathViejo = './server./uploads/categories/' + category.cat_img;
-                var pathViejo = './uploads/categories/' + category.cat_img;
+                var pathViejo = __dirname + './uploads/categories/' + category.cat_img;
 
                 // console.log('el path viejo es: ' + pathViejo+'espaciopegado');
 
@@ -331,7 +331,7 @@ app.put('/:tipo/:id', (req, res) => {
                 console.log(subcategory.subcat_img);
 
                 // var pathViejo = './server./uploads/subcategories/' + subcategory.subcat_img;
-                var pathViejo = './uploads/subcategories/' + subcategory.subcat_img;
+                var pathViejo = __dirname + './uploads/subcategories/' + subcategory.subcat_img;
 
                 // console.log('el path viejo es: ' + pathViejo+'espaciopegado');
 
@@ -403,7 +403,7 @@ app.put('/:tipo/:id', (req, res) => {
                 console.log(product.prod_img);
 
                 // var pathViejo = './server./uploads/products/' + product.prod_img;
-                var pathViejo = './uploads/products/' + product.prod_img;
+                var pathViejo = __dirname + './uploads/products/' + product.prod_img;
 
                 // console.log('el path viejo es: ' + pathViejo+'espaciopegado');
 
