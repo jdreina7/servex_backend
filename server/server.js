e7f35d01a9d9e7c58f2324ebee90c53c7c4dc04e
Juan David Reina <judareiro@gmail.com>
2019-11-18 23:38:41 -0500

    Comentando cors

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ const bodyParser = require('body-parser')
 const app = express()
 
 // CORS
-app.use( function( req, res, next ) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	next();
-});
+// app.use( function( req, res, next ) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Credentials", true);
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+// 	next();
+// });
 
-app.use(cors());
+// app.use(cors());
 
 
 __dirname = path.resolve(path.dirname(''));
